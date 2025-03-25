567b6483ae89da533201ddcce6db4cf8d5d899dd
Seny BALDE <seny.balde1@unchk.edu.sn>
2025-03-25 00:40:38 -1200

    article page updated

--- fireshieldsec_admin_v1/src/app/admin/article/page.tsx
+++ fireshieldsec_admin_v1/src/app/admin/article/page.tsx
@@ -13,13 +13,20 @@ import tableDataTopCreators from 'variables/nfts/marketplace/tableDataTopCreator
 import HistoryItem from 'components/admin/nft-marketplace/HistoryItem';
 import TopCreatorTable from 'components/admin/nft-marketplace/TableTopCreators';
 import NftCard from 'components/card/NftCard';
+import Banner1 from 'components/admin/nft-marketplace/Banner';
 
 const Marketplace = () => {
   return (
     <div className="mt-3 grid h-full grid-cols-1 gap-5 xl:grid-cols-2 2xl:grid-cols-3">
       <div className="col-span-1 h-fit w-full xl:col-span-1 2xl:col-span-2">
         {/* NFt Banner */}
-        <Banner />
+        <Banner1 
+        
+        title="Welcome to the NFT Marketplace"
+  content="Discover the latest NFTs and trends in the market."
+  buttonOne={{ title: "Explore Now", link: "/explore" }}
+  buttonTwo={{ title: "Learn More", link: "/learn-more" }}
+        />
 
         {/* NFt Header */}
         <div className="mb-4 mt-5 flex flex-col justify-between px-4 md:flex-row md:items-center">
